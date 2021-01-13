929420d155a56ad033b2ec21eabcbcce83ea660b
Iain Stewart <iain.stewart.08@gmail.com>
2021-01-13 00:16:27 +0000

    Add Math tests

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    public class MathTests
+    {
+        private Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+        public void Add_OnePlusOne_ReturnsTwo()
+        {
+            var result = _math.Add(1, 1);
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnsFirstArgument()
+        {
+            var result = _math.Max(2, 1);
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnsSecondArgument()
+        {
+            var result = _math.Max(1, 2);
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnsSameArgument()
+        {
+            var result = _math.Max(1, 1);
+            Assert.That(result, Is.EqualTo(1));
+        }
+    }
+}
