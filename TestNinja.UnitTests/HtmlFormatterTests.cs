3941660edb9572db477d35b450aad21659c0044a
Iain Stewart <iain.stewart.08@gmail.com>
2021-01-13 22:36:33 +0000

    Add HtmlFormatter test

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenGivenAlphanumericString_ReturnsStringSurroundedByStrongTags()
+        {
+            var htmlFormatter = new HtmlFormatter();
+            var result = htmlFormatter.FormatAsBold("Test123");
+            Assert.That(result, Is.EqualTo("<strong>Test123</strong>").IgnoreCase);
+        }
+    }
+}
