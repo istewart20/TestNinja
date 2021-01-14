78d7e0a4ea9a6361825fa62bc55458140ff74b68
Iain Stewart <iain.stewart.08@gmail.com>
2021-01-14 23:15:44 +0000

    Add CustomerController tests

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var customerController = new CustomerController();
+            var result = customerController.GetCustomer(0);
+            Assert.That(result, Is.TypeOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnNotOk()
+        {
+            var customerController = new CustomerController();
+            var result = customerController.GetCustomer(1);
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
