4e3123d1ce2d42084f1e7caf24311b92b4df0ba4
Iain Stewart <iain.stewart.08@gmail.com>
2021-01-20 00:05:33 +0000

    Add error logger test

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetsTheLastErrorProperty()
+        {
+            var errorLogger = new ErrorLogger();
+            errorLogger.Log("test");
+            Assert.That(errorLogger.LastError, Is.EqualTo("test"));
+        }
+    }
+}
