36dcf1154b5ba017bc0c589f8a38375a40cf9910
Iain Stewart <iain.stewart.08@gmail.com>
2021-01-21 00:28:23 +0000

    Add FixxBuzz tests

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_DividesBy3And5_ReturnsFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_DividesBy3_ReturnsFizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_DividesBy5_ReturnsBuzz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutput_DoesNotDivideBy3Or5_ReturnsInputNumberAsString()
+        {
+            var result = FizzBuzz.GetOutput(4);
+            Assert.That(result, Is.EqualTo("4"));
+        }
+    }
+}
